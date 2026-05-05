315f5aba5fc820928bb6d4ad39bbf46636d793b6
Max <maxcascerceri@verizon.net>
2026-05-05 19:41:01 -0400

    Initial Commit

---
+++ ASL/ASLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ASLApp.swift
+//  ASL
+//
+//  Created by Max Cascerceri on 5/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ASLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ASL/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ASL
+//
+//  Created by Max Cascerceri on 5/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
